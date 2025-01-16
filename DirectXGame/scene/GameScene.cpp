d26e2bfcf18665350a2955fe6455375e3aea9460
OKUBO-0 <k023g0020@g.neec.ac.jp>
2025-01-16 17:08:32 +0900

    エネミー仮置き

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -20,7 +20,7 @@ void GameScene::Initialize() {
 	NormalBlock = Model::CreateFromOBJ("cube", true);
 	BomBlock = Model::CreateFromOBJ("bomblock", true);
 	SlimeBlock = Model::CreateFromOBJ("slime", true);
-	EnemyBlock = Model::CreateFromOBJ("enemyblock", true);
+	EnemyBlock = Model::CreateFromOBJ("enemy", true);
 	GoalBlock = Model::CreateFromOBJ("goalblock", true);
 
 	// マップチップフィールドの生成
